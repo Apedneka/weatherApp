4f4f33419d82321cebe2fdb0664eef8c53b3e1bc
apedneka <pednekar.ameya@gmail.com>
2015-12-06 21:44:21 -0500

    express routing working, JSON data available on localhost::3000/forecast

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 var Wunderground = require('wunderground-api');
 var client = new Wunderground('d8483e016960a875', '10035');
 
- 
-client.conditions('', function(err, data) {
+var express = require ('express');
+var app = express();
+
+// /forecast displays JSON data about the current forecast, pulled from Wunderground 
+app.get('/forecast',function(req,res){
+	client.conditions('', function(err, data) {
   if (err) throw err;
-  else console.log(data);
+  else res.send(data);
 });
+	
+});
+
+app.listen(3000);
+console.log('listening on port 3000');
