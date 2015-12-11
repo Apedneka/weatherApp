02dab0a1c65dcb4afdb38b67e26d90c85a472477
apedneka <pednekar.ameya@gmail.com>
2015-12-11 10:52:07 -0500

    added ejs view engine

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ var client = new Wunderground('d8483e016960a875', 'San Fransisco');
 var express = require ('express');
 var app = express();
 
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
 // /forecast displays JSON data about the current forecast, pulled from Wunderground 
 app.get('/forecast',function(req,res){
 	client.conditions('', function(err, data) {
@@ -11,6 +14,10 @@ app.get('/forecast',function(req,res){
   else res.send(data);
 });
 	
+});
+
+app.get('/test',function(req,res){
+	res.render('index')
 });
 
 
